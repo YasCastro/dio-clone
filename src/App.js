662bc6b779d5c8ed49907e984be586a5fb8d3328
Yasmine C. Fortes <yazzcastrof@gmail.com>
2023-09-17 00:29:10 -0300

    tela de cadastro

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import {
 } from "react-router-dom";
 import { Feed } from "./pages/Feed";
 
-import { Home } from './pages/Home'
-import { Login } from './pages/Login'
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Cadastro } from './pages/Cadastro'
 import { GlobalStyle } from './styles/global';
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/feed" element={<Feed />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+
      </Routes >
     </Router>
   );
